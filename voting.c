39ab027b66237f613b13d18b5619a558004f14c5
Debanga Raz Hazarika <debangaraz2000@gmail.com>
2022-04-03 02:13:49 +0530

    ver4

---
+++ voting.c
@@ -0,0 +1,23 @@
+#include<string.h>
+int main()
+{
+    int age;
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    if (18 < age)
+    {
+        printf("You are Elgible");
+    }
+    if (18 = age)
+    {
+        printf("You are elgible");
+    }
+    
+    
+    else
+    {
+        printf("You are not Elgible");
+    }
+}   
