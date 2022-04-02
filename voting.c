c42da558de4927c619ba741161db8f15b58af2a4
Debanga Raz Hazarika <debangaraz2000@gmail.com>
2022-04-03 03:00:38 +0530

    Ver8

--- voting.c
+++ voting.c
@@ -2,23 +2,29 @@
 int main()
 {
     int age;
+    char Name[100];
+
+    printf("Welcome to ECI, What is your name?\n>");
+    scanf("%s", &Name);
 
     printf("Enter your age: ");
     scanf("%d", &age);
 
+    printf("\n\n")
+
     if (18 < age)
     {
-        printf("You are Elgible");
+        printf("%s,you are Elgible",Name);
     }
     if (18 == age)
     {
-        printf("You are elgible");
+        printf("%s,you are Elgible",Name);
     }
     
     
     else
     {
-        printf("You are not Elgible");
+        printf("%s,you are not elgible",Name);
     }
 
     printf("\n\nThanks For using us");
